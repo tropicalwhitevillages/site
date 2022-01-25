283a3f60e3028d6fe4daea1df23668365cd84d39
marco-giuliani <48715348+marco-giuliani@users.noreply.github.com>
2022-01-25 17:22:53 +0000

    fuse options restored

--- assets/plugins/search/search.js
+++ assets/plugins/search/search.js
@@ -1,18 +1,33 @@
 summaryInclude = 100;
 var fuseOptions = {
+  isCaseSensitive: false,
+  includeScore: false,
   shouldSort: true,
   includeMatches: true,
-  threshold: 0.0,
-  tokenize:true,
+  findAllMatches: false,
+  minMatchCharLength: 3,
   location: 0,
-  distance: 100,
-  maxPatternLength: 32,
-  minMatchCharLength: 1,
-  keys: [
-    {name:"title",weight:0.8},
-    {name:"contents",weight:0.5},
-    {name:"tags",weight:0.3},
-    {name:"categories",weight:0.3}
+  threshold: 0.5,
+  distance: 50,
+  useExtendedSearch: false,
+  ignoreLocation: false,
+  ignoreFieldNorm: false,
+  keys: [{
+      name: "title",
+      weight: 0.8
+    },
+    {
+      name: "tags",
+      weight: 0.5
+    },
+    {
+      name: "categories",
+      weight: 0.5
+    },
+    {
+      name: "contents",
+      weight: 0.3
+    }
   ]
 };
 
